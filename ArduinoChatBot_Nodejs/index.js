14d5d9dc44a654a8e189cc00106f0daeeff7ef97
chepecarlos <chepecarlos@alswblog.org>
2018-04-11 22:10:36 -0600

    Codigo final de la app de nodejs

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -4,16 +4,35 @@ const token = '598936675:AAFCIFQoi6w9O8AXslgidlAii5LZ_GF8o7M';
 const bot = new TelegramBot(token, {
   polling: true
 });
+var IdMiChat = 30085334;
+
+var SerialPort = require('serialport');
+var MiPuerto = new SerialPort('/dev/ttyUSB0', {
+  baudRate: 9600,
+  autoOpen: true
+});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  console.log("El ID del char" + chatId);
   var Mensaje = msg.text;
-  if (Mensaje == "encender") {
+  if (Mensaje == "Encender") {
     console.log("encendiendo el led");
     bot.sendMessage(chatId, 'Encendere el led');
-  } else if (Mensaje == "apagar") {
+    MiPuerto.write("H");
+  } else if (Mensaje == "Apagar") {
     console.log("apagar el led");
     bot.sendMessage(chatId, 'Apagare el led');
+    MiPuerto.write("L");
   }
+});
 
+MiPuerto.setEncoding('utf8');
+
+MiPuerto.on('data', function(data) {
+  console.log("Lo que entro es " + data);
+  if (data[0] == 'H') {
+    console.log("Boton Precionado");
+    bot.sendMessage(IdMiChat, "Se preciono el boton");
+  }
 });
